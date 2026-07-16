6ebb6c2bf35dd084103b04df23e530b292886fb7
AltmzTrn <101617854+AltmzTrn@users.noreply.github.com>
2026-07-16 21:27:36 +0700

    UGHHH I'M IN A FOREST FULL OF THE CODES WHERE TF IS THE REAL ONE

--- Quadcopter/Brushed/CRSFHandler.cpp
+++ Quadcopter/Brushed/CRSFHandler.cpp
@@ -1,48 +1,32 @@
 #include "CRSFHandler.h"
 
+//commented out because I wanna make this code modular for both STM31F1XX & STM32F4XX series but right now I'm still working on the STM32F103 version.
+//HardwareSerial crsfSerial(PB11, PB10); // alternate should be "PA3, PA2" but ts pmo icl :broken_heart:
+
 int rcChannelValues[crsfProtocol::RC_CHANNEL_COUNT] = {0};
 bool crsfFailsafe = true;
 
 #define CRSF_ADDRESS_FLIGHT_CONTROLLER 0xC8
 #define CRSF_FRAME_RC_CHANNELS_PACKED  0x16
-#define CRSF_FRAME_LENGTH              26 // 1 (addr) + 1 (len) + 1 (type) + 22 (payload) + 1 (crc)
+#define CRSF_FRAME_LENGTH              26 // 1 (addr) + 1 (len) + 1 (type) + 22 (payload)
 
 static uint8_t buffer[CRSF_FRAME_LENGTH];
 static uint8_t bufferIndex = 0;
-
-#define CRSF_RXBUF_SIZE 64
-static volatile uint8_t rxBuf[CRSF_RXBUF_SIZE];
-static volatile uint8_t rxHead = 0;
-static volatile uint8_t rxTail = 0;
-
-extern "C" void USART3_IRQHandler(void) {
-    if (USART3->SR & USART_SR_RXNE) {
-        uint8_t b = USART3->DR; // reading DR returns the byte AND clears RXNE
-        uint8_t next = (rxHead + 1) % CRSF_RXBUF_SIZE;
-        if (next != rxTail) { // drop the byte if the ring buffer is full, don't overwrite unread data
-            rxBuf[rxHead] = b;
-            rxHead = next;
-        }
-    }
-}
 
 void crsf_setup() {
     RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
     RCC->APB1ENR |= RCC_APB1ENR_USART3EN;
 
-    GPIOB->CRH &= ~((0xFu << 8) | (0xFu << 12));
-    GPIOB->CRH |=  (0xBu << 8) | (0x4u << 12); // PB10 AF push-pull 50MHz (TX), PB11 floating input (RX)
+    GPIOB->CRH &= ~((0xFu<<8)|(0xFu<<12));
+    GPIOB->CRH |= (0xBu<<8)|(0x4u<<12); // PB10 AF push-pull 50MHz (TX), PB11 floating input (RX)
 
-    USART3->BRR = 86; // ~420000 baud @ 36MHz PCLK1 (~0.33% error)
-    USART3->CR1 = USART_CR1_UE | USART_CR1_TE | USART_CR1_RE | USART_CR1_RXNEIE;
-
-    NVIC_EnableIRQ(USART3_IRQn);
+    USART3->BRR = 86; //~420000 baud @ 36MHz PCLK1 (~0.33% error)
+    USART3->CR1 = USART_CR1_UE|USART_CR1_TE|USART_CR1_RE;
 }
 
 void crsf_update() {
-    while (rxTail != rxHead) {
-        uint8_t byteIn = rxBuf[rxTail];
-        rxTail = (rxTail + 1) % CRSF_RXBUF_SIZE;
+    while (USART3->SR & USART_SR_RXNE) {
+        uint8_t byteIn = USART3->DR;
 
         buffer[bufferIndex++] = byteIn;
 
