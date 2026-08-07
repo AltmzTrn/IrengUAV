eac558a77b65e87dbda0ca44bd794754e2da71fc
AltmzTrn <101617854+AltmzTrn@users.noreply.github.com>
2026-08-07 09:18:37 +0700

    Functionality commit

--- Helicopter/Virtual-Swashplate Controls/Conventional/actuators.cpp
+++ Helicopter/Virtual-Swashplate Controls/Conventional/actuators.cpp
@@ -1,28 +1,28 @@
 #include "actuators.h"
 #include "controlSystems.h"
 
-/*void writeServoMicros(uint8_t pin, uint16_t us) {
+void writeServoMicros(uint8_t pin, uint16_t us) {
   const uint32_t pwmFreq = 50;    // 50 Hz for servos and ESC
   const uint32_t timerPeriod = 1000000 / pwmFreq; // Period in microseconds (20ms)
 
   // Convert desired pulse width to duty
   uint32_t duty = (us * 65535UL) / timerPeriod;
   analogWrite(pin, duty);
-}*/
+}
 
 void actuators_setup() {
   pinMode(MainRotorPin, OUTPUT);
   pinMode(TailRotorPin, OUTPUT);
 
-  /*analogWriteResolution(16);  // 0 - 65535, shared by both pins
-  analogWriteFrequency(50);*/
+  analogWriteResolution(16);  // 0 - 65535, shared by both pins
+  analogWriteFrequency(50);
 }
 
 
 void actuators_write() {
   // Write directly as microsecond PWM signals
-  analogWrite(MainRotorPin, to_actuator[0]);
-  analogWrite(TailRotorPin, 0);
+  writeServoMicros(MainRotorPin, to_actuator[0]);
+  analogWrite(TailRotorPin, to_actuator[1]);
 }
 
 void encoder_read() {
